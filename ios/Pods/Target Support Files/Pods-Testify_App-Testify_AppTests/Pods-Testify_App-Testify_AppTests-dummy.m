f4e066a9965165a9ba34f76cb82ba1d00cffb832
gargnavi91 <navdeepgargwins@gmail.com>
2022-09-04 16:26:04 +0530

    Drop files from .gitignore

--- ios/Pods/Target Support Files/Pods-Testify_App-Testify_AppTests/Pods-Testify_App-Testify_AppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Testify_App_Testify_AppTests : NSObject
-@end
-@implementation PodsDummy_Pods_Testify_App_Testify_AppTests
-@end
--- ios/Pods/Target Support Files/Pods-Testify_App/Pods-Testify_App-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Testify_App : NSObject
-@end
-@implementation PodsDummy_Pods_Testify_App
-@end
